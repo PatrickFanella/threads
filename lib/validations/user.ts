ea887af8e73ed2ac94d39bb4d8f258a8d02c5300
patrickfanella <fanella.patrick@gmail.com>
2023-09-08 21:04:38 -0500

    end of day, working on onboarding forms

--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -2,6 +2,7 @@ import * as z from 'zod'
 
 export const userValidation = z.object({
     profile_photo: z.string().url().nonempty(),
-    name: z.string().min(3, 'minimum 3 char').max(30).nonempty(),
-    bio: z.string().min(3).max(1000).nonempty(),
+    name: z.string().min(3).max(30),
+    username: z.string().min(3).max(30)
+    bio: z.string().min(3).max(1000),
 })
